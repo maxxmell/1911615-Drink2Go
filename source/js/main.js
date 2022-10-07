75b80336d9f517826759d9ec50331e9f2c8139db
Sergey <Maxxmell@yandex.ru>
2022-10-07 11:46:55 +0300

    slider layout added

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,35 @@
 import renderMap from './map.js';
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
 const map = renderMap({
   center: [59.968307, 30.317532],
   zoom: 20
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  // effect: "creative",
+  // creativeEffect: {
+  //   prev: {
+  //     translate: ["-20%", 0, -1],
+  //   },
+  //   next: {
+  //     translate: ["100%", 0, 0],
+  //   },
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: true,
 });
 
 // map.whenReady(async () => {
