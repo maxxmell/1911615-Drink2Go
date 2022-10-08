422c92404782593027d8d8e11860dbcb9daaa64f
Sergey <Maxxmell@yandex.ru>
2022-10-08 23:07:51 +0300

    best deal + features styles completed

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,10 @@
 import renderMap from './map.js';
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
-const map = renderMap({
-  center: [59.968307, 30.317532],
-  zoom: 20
-});
+// const map = renderMap({
+//   center: [59.968307, 30.317532],
+//   zoom: 20
+// });
 
 const swiper = new Swiper(".swiper", {
   loop: true,
@@ -12,18 +12,9 @@ const swiper = new Swiper(".swiper", {
   //   delay: 2500,
   //   disableOnInteraction: false,
   // },
-  // effect: "creative",
-  // creativeEffect: {
-  //   prev: {
-  //     translate: ["-20%", 0, -1],
-  //   },
-  //   next: {
-  //     translate: ["100%", 0, 0],
-  //   },
-  // },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".best-deal__button--next",
+    prevEl: ".best-deal__button--prev",
   },
   pagination: {
     el: ".swiper-pagination",
