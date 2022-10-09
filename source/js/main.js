80ff9a73f0ae349dd27534ad9560da984f64509f
Sergey <Maxxmell@yandex.ru>
2022-10-09 07:57:54 +0300

    As much as i can for today

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,10 @@
 import renderMap from './map.js';
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
-// const map = renderMap({
-//   center: [59.968307, 30.317532],
-//   zoom: 20
-// });
+const map = renderMap({
+  center: [59.968307, 30.317532],
+  zoom: 20
+});
 
 const swiper = new Swiper(".swiper", {
   loop: true,
@@ -23,17 +23,3 @@ const swiper = new Swiper(".swiper", {
   keyboard: true,
 });
 
-// map.whenReady(async () => {
-//   try {
-//     const ads = await getAds();
-//     const replacePins = debounce(() => map.replacePins(mapForm.filter(ads)));
-
-//     mapForm.setDisabled(false).on('change', replacePins).fire('change');
-
-//   } catch (exception) {
-//     showMessage('error',`Ошибка: ${exception.status || exception.message}`);
-//   }
-
-//   adForm.setDisabled(false);
-//   map.primaryPin.fire('move');
-// });
--- source/js/map.js
+++ source/js/map.js
@@ -22,20 +22,6 @@ function renderMap(options) {
     zIndexOffset: 1000
   });
 
-  // /**
-  //  * Изображение второстепенной метки.
-  //  */
-  // const secondaryPinIcon = L.icon({
-  //   iconUrl: './img/pin.svg',
-  //   iconSize: [40, 40],
-  //   iconAnchor: [20, 40]
-  // });
-
-  // /**
-  //  * Группа второстепенных меток.
-  //  */
-  // const secondaryPinGroup = L.layerGroup();
-
   /**
    * Картографический слой.
    */
@@ -57,29 +43,6 @@ function renderMap(options) {
     primaryPin,
     primaryPinIcon,
     tileLayer,
-
-    // /**
-    //  * Добавит метку объявления на карту.
-    //  * @param {Ad} ad
-    //  */
-    // appendPin(ad) {
-    //   const pin = L.marker(ad.location, {
-    //     icon: secondaryPinIcon
-    //   });
-    //   pin.bindPopup(createPopupElement(ad));
-
-    //   secondaryPinGroup.addLayer(pin);
-    // },
-
-    // /**
-    //  * Заменит существующие метки объявлений на карте.
-    //  * @param {Ad[]} ads
-    //  */
-    // replacePins(ads) {
-    //   secondaryPinGroup.clearLayers();
-
-    //   ads.forEach(this.appendPin);
-    // },
 
     /**
      * Вернет местоположение и масштаб в исходное состояние.
